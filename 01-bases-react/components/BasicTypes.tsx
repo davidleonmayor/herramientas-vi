e84872a766d0d582733d183ccd7ee16901fa9c1b
davidleonmayor <jd.leon@unimayor.edu.co>
2025-08-19 11:43:32 -0500

    data types

--- 01-bases-react/components/BasicTypes.tsx
+++ 01-bases-react/components/BasicTypes.tsx
@@ -1,6 +1,56 @@
-import react from "react";
+import {useState} from "react";
 
-export const BasicTypes = () => {
+export function BasicTypes() {
+  // variables
+  const name: string = "David";
+  const age: number = 28;
+  const isActive: boolean = true;
 
-  return ("hello")
+  // listas
+  const names: string[] = ["Julian", "Victor", "Dhayana", "San"];
+
+  const [active, setActive] = useState(isActive);
+
+  return (
+    <div className="">
+    <h2>Variables</h2>
+      <ol>
+        <li>Name: {name}</li>
+        <li>Age: {age}</li>
+        <li>is acetive: {active? "yes": "no"}</li>
+      </ol>
+
+      <hr />
+      
+      {/* ul>li{"nombre $"}*4 */}
+      <h2> Lista de nombres</h2>
+      <ul>
+      {
+        names.map((name, index) => (
+          <li key={index}>
+            {name}
+          </li>
+        ))
+      }
+      </ul>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <h1>ME GUSTA EL PENE GIGANTE</h1>
+
+
+      <button onClick={() => { console.log(active); setActive(!active)}}>Change</button>
+    </div>
+  );
 }
